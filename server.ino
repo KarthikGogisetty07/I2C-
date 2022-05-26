236f01c34b20ab5ea9e59c65912516b0be89c765
Karthik <69350191+KarthikGogisetty07@users.noreply.github.com>
2022-05-26 16:59:40 +0530

    Add files via upload

--- server.ino
+++ server.ino
@@ -5,15 +5,16 @@ boolean lastState = LOW;
 //int x;
 void setup() {
   // put your setup code here, to run once:
- pinMode(buttonPin, INPUT_PULLUP); 
+ pinMode(buttonPin, INPUT); 
  Wire.begin();
+ Serial.begin(9600);
 }
 
 void loop() {
   delay(100);
   // put your main code here, to run repeatedly:
   buttonAction = digitalRead(buttonPin);
-  if (buttonAction != lastState){
+  if ((buttonAction && lastState) == LOW){
     lastState = buttonAction;
     //if (buttonAction == HIGH){
       //x = 1;
